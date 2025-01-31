c9d96ad9284ba3b025be8b5c21a73ced068e5173
kadiegonzales <gonzalesmelissak1010@gmail.com>
2025-01-31 17:20:00 +0000

    release version 1.0

--- finalkadie.py
+++ finalkadie.py
@@ -8,11 +8,6 @@ import json
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
 
-
-db = firestore.Client.from_service_account_json("proyectofinalkadie-firebase-adminsdk-fbsvc-49bb139411.json")
-
-
-
 # Initialize Firestore client
 db = firestore.Client(credentials=creds, project="PROYECTOFINALKADIE")
 dbNames = db.collection("name")
